6574120fe1668be417c2fb97a7638d8e0dcdf9e4
Ab09090909 <167700907+Ab09090909@users.noreply.github.com>
2026-07-11 21:01:59 +0300

    Add tab navigation for customer features

--- pages/3_customer.py
+++ pages/3_customer.py
@@ -2,6 +2,9 @@ import streamlit as st
 from utils.nav import render_sidebar
 from utils.chatbot import render_chatbot
 from customer.home import render_customer_home
+from customer.marketplace import render_customer_marketplace
+from customer.recommendations import render_recommendations
+from customer.favorites import render_favorites
 
 st.set_page_config(page_title="Customer Home", page_icon="🛍️", layout="wide")
 
@@ -9,5 +12,15 @@ if st.session_state.get("role") != "customer":
     st.switch_page("app.py")
 
 render_sidebar()
-render_customer_home()
+
+tab = st.session_state.get("current_tab", "Home")
+if tab == "Home":
+    render_customer_home()
+elif tab == "Marketplace":
+    render_customer_marketplace()
+elif tab == "For You":
+    render_recommendations()
+elif tab == "Favorites":
+    render_favorites()
+
 render_chatbot()
